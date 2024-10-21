7742ca5f83e51383abb25def70c7c657f80dbdab
husainarib <husainarib@gmail.com>
2024-10-21 19:59:28 -0400

    CW 05 COMPLETE

--- cw05/sucker.php
+++ cw05/sucker.php
@@ -1,12 +1,20 @@
 <?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-	$name = htmlspecialchars($_POST["student_name"]);
-	$section = htmlspecialchars($_POST["section"]);
-	$cc_number = htmlspecialchars($_POST["cc_number"]);
-	$cc_type = htmlspecialchars($_POST["cc_type"]);
+function isValid($data)
+{
+	return isset($data) && trim($data) !== '';
+}
+
+$name = $_POST["student_name"];
+$section = $_POST["section"];
+$cc_number = $_POST["cc_number"];
+$cc_type = $_POST["cc_type"];
+
+if (isValid($name) && isValid($section) && isValid($cc_number) && isValid($cc_type)) {
+	$entry = "$name;$section;$cc_number;$cc_type\n";
+	file_put_contents("suckers.html", $entry, FILE_APPEND);
+	$all_entries = file_get_contents("suckers.html");
 } else {
-	header("Location: buyagrade.html");
-	exit();
+	$error = "You didn't fill out the form completely. Try again.";
 }
 ?>
 
@@ -19,20 +27,30 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 </head>
 
 <body>
-	<h1>Thanks, sucker!</h1>
+	<?php if (isset($error)): ?>
+		<h1>Sorry, sucker!</h1>
+		<p><?php echo $error; ?></p>
+		<p><a href="buyagrade.html">Try again</a></p>
+	<?php else: ?>
+		<h1>Thanks, sucker!</h1>
 
-	<p>Your information has been recorded.</p>
+		<p>Your information has been recorded.</p>
 
-	<dl>
-		<dt>Name</dt>
-		<dd><?php echo $name; ?></dd>
+		<dl>
+			<dt>Name</dt>
+			<dd><?php echo $name; ?></dd>
 
-		<dt>Section</dt>
-		<dd><?php echo $section; ?></dd>
+			<dt>Section</dt>
+			<dd><?php echo $section; ?></dd>
 
-		<dt>Credit Card</dt>
-		<dd><?php echo $cc_number; ?> (<?php echo $cc_type; ?>)</dd>
-	</dl>
+			<dt>Credit Card</dt>
+			<dd><?php echo $cc_number; ?> (<?php echo $cc_type; ?>)</dd>
+		</dl>
+
+		<p>Here are all the suckers who have submitted here:</p>
+
+		<pre><?php echo $all_entries; ?></pre>
+	<?php endif; ?>
 </body>
 
 </html>
