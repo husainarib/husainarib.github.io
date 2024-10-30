968ddebef8a5fab8b895120d910ad9717e3c919c
husainarib <husainarib@gmail.com>
2024-10-29 22:29:41 -0400

    create index.php and matches.php.

---
+++ hw3/index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>NerdLuv - Welcome</title>
+    <link rel="stylesheet" href="nerdieluv.css">
+</head>
+
+<body>
+    <div id="bannerarea">
+        <h1>nerdLuv<sup>TM</sup></h1>
+        <p>where meek geeks meet</p>
+    </div>
+
+    <h2>Welcome!</h2>
+
+    <ul>
+        <li>
+            <a href="matches.php"><img src="matches.png" alt="Matches icon"> Check your matches</a>
+        </li>
+    </ul>
+
+    <p>This page is for single nerds to meet and date each other! Type in your personal information and wait for the nerdly luv to begin! Thank you for using our site.</p>
+    <p>Results and page (C) Copyright NerdLuv Inc.</p>
+
+    <div id="w3c">
+        <p>
+            <img src="w3c-html5.png" alt="HTML5 compliant">
+            <img src="w3c-css.png" alt="CSS compliant">
+        </p>
+    </div>
+</body>
+
+</html>
---
+++ hw3/matches.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>NerdLuv - Find Your Match</title>
+    <link rel="stylesheet" href="nerdieluv.css">
+</head>
+
+<body>
+    <div id="bannerarea">
+        <h1>nerdLuv<sup>TM</sup></h1>
+        <p>where meek geeks meet</p>
+    </div>
+
+    <form action="matches-submit.php" method="get">
+        <fieldset>
+            <legend>Returning User:</legend>
+            <label for="name" class="left"><strong>Name:</strong></label>
+            <input type="text" id="name" name="name" maxlength="16">
+            <input type="submit" value="View My Matches">
+        </fieldset>
+    </form>
+
+    <p>This page is for single nerds to meet and date each other! Type in your personal information and wait for the nerdly luv to begin! Thank you for using our site.</p>
+    <p>Results and page (C) Copyright NerdLuv Inc.</p>
+
+    <p>
+        <a href="index.php"><img src="back.png" alt="Back to front page"> Back to front page</a>
+    </p>
+
+    <div id="w3c">
+        <p>
+            <img src="w3c-html5.png" alt="HTML5 compliant">
+            <img src="w3c-css.png" alt="CSS compliant">
+        </p>
+    </div>
+</body>
+
+</html>
