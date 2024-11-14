987bd749c58b9887ff7ab889f66fea40e44b750a
husainarib <husainarib@gmail.com>
2024-11-13 21:46:56 -0500

    Part V: Add a Clear Button to the Page CW07

--- cw07/nonograms.js
+++ cw07/nonograms.js
@@ -8,4 +8,15 @@ document.addEventListener("DOMContentLoaded", function () {
       tile.classList.toggle("filled");
     });
   });
+
+  const clearButton = document.getElementById("clear-button");
+
+  // Attach a click event listener to the Clear button
+  clearButton.addEventListener("click", function () {
+    if (confirm("Are you sure you want to clear all tiles?")) {
+      tiles.forEach((tile) => {
+        tile.classList.remove("filled");
+      });
+    }
+  });
 });
