b44f43f1b7e45dfc7866d901afe95722dbe59e1a
husainarib <husainarib@gmail.com>
2024-11-19 12:30:18 -0500

    add images for part b script, and link part B to part A

---
+++ hw4/partbscript.js
@@ -0,0 +1,26 @@
+const images = [
+  "partb_imgs/car1.jpg",
+  "partb_imgs/car2.jpg",
+  "partb_imgs/car3.jpg",
+  "partb_imgs/car4.jpg",
+  "partb_imgs/car5.jpg",
+  "partb_imgs/car6.jpg",
+  // twice the same image
+  "partb_imgs/car1.jpg",
+  "partb_imgs/car2.jpg",
+  "partb_imgs/car3.jpg",
+  "partb_imgs/car4.jpg",
+  "partb_imgs/car5.jpg",
+  "partb_imgs/car6.jpg",
+];
+
+let gameCards = [];
+let selectedCards = [];
+let matchedPairs = 0;
+let timer;
+let timeRemaining;
+let memorizeTimer;
+
+function startGame(cardCount) {
+  // TODO START GAME LOGIC
+}
